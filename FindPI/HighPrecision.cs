2edee245655fcd3f05e212f5e3f4cae2aa0de726
afarkas2508 <32510769+afarkas2508@users.noreply.github.com>
2017-10-04 11:48:30 +0200

    Initial commit

---
+++ FindPI/HighPrecision.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPI
+{
+    /// <summary>
+    /// HighPrecision class by Lincoln Atkinson.
+    /// See more at: http://www.latkin.org/blog/2012/03/20/how-to-calculate-1-million-digits-of-pi/
+    /// </summary>
+    public class HighPrecision
+    {
+        #region Private variables
+
+        private static BigInteger denom;
+        private static int precision;
+        private static int slop = 4;
+        private BigInteger num;
+
+        #endregion
+
+        #region Constructors
+
+        public HighPrecision(BigInteger numerator, BigInteger denominator)
+        {
+            // public constructor rescales numerator as needed
+            num = (HighPrecision.denom * numerator) / denominator;
+        }
+
+        private HighPrecision(BigInteger numerator)
+        {
+            // private constructor for when we already know the scaling
+            num = numerator;
+        }
+
+        #endregion
+
+        #region Public properties
+
+        public static int Precision
+        {
+            get { return precision; }
+            set
+            {
+                HighPrecision.precision = value;
+                denom = BigInteger.Pow(10, HighPrecision.precision + slop);  // leave some buffer
+            }
+        }
+
+        public bool IsZero
+        {
+            get { return num.IsZero; }
+        }
+
+        public BigInteger Numerator
+        {
+            get { return num; }
+        }
+
+        public BigInteger Denominator
+        {
+            get { return HighPrecision.denom; }
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static HighPrecision operator *(int left, HighPrecision right)
+        {
+            // no need to resale when multiplying by an int
+            return new HighPrecision(right.num * left);
+        }
+
+        public static HighPrecision operator *(HighPrecision left, HighPrecision right)
+        {
+            // a/D * b/D = ab/D^2 = (ab/D)/D
+            return new HighPrecision((left.num * right.num) / HighPrecision.denom);
+        }
+
+        public static HighPrecision operator /(HighPrecision left, int right)
+        {
+            // no need to rescale when dividing by an int
+            return new HighPrecision(left.num / right);
+        }
+
+        public static HighPrecision operator +(HighPrecision left, HighPrecision right)
+        {
+            // when we know the denominators are the same, can just add the numerators
+            return new HighPrecision(left.num + right.num);
+        }
+
+        public static HighPrecision operator -(HighPrecision left, HighPrecision right)
+        {
+            // when we know the denominators are the same, can just subtract the numerators
+            return new HighPrecision(left.num - right.num);
+        }
+
+        #endregion
+        
+        #region Overriden methods
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // pull out the integer part
+            BigInteger remain;
+            BigInteger quotient = BigInteger.DivRem(num, HighPrecision.denom, out remain);
+            int integerDigits = quotient.IsZero ? 1 : (int)BigInteger.Log10(quotient) + 1;
+            sb.Append(quotient.ToString());
+
+            int i = 0;
+            BigInteger smallDenom = HighPrecision.denom / 10;
+            BigInteger tempRemain;
+
+            // pull out all of the 0s after the decimal point
+            while (i++ < HighPrecision.Precision && (quotient = BigInteger.DivRem(remain, smallDenom, out tempRemain)).IsZero)
+            {
+                smallDenom /= 10;
+                remain = tempRemain;
+                sb.Append('0');
+            }
+
+            // append the rest of the remainder
+            sb.Append(remain.ToString());
+
+            // truncate and insert the decimal point
+            return sb.ToString().Remove(integerDigits + HighPrecision.Precision).Insert(integerDigits, ".");
+        }
+
+        #endregion
+        
+        #region Static methods
+
+        public static HighPrecision GetPi(int digits)
+        {
+            HighPrecision.Precision = digits;
+            HighPrecision first = 4 * Atan(5);
+            HighPrecision second = Atan(239);
+            return 4 * (first - second);
+        }
+
+        public static HighPrecision Atan(int denominator)
+        {
+            HighPrecision result = new HighPrecision(1, denominator);
+            int xSquared = denominator * denominator;
+
+            int divisor = 1;
+            HighPrecision term = result;
+
+            while (!term.IsZero)
+            {
+                divisor += 2;
+                term /= xSquared;
+                result -= term / divisor;
+
+                divisor += 2;
+                term /= xSquared;
+                result += term / divisor;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ FindPI/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPI
+{
+    class Program
+    {
+        static readonly char[] acceptedAnswers = new char[] { 'y', 'Y', 'n', 'N' };
+        static readonly uint numberOfDecimalPlacesToWarnUser = 100000;
+
+        /// <summary>
+        /// Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                // Get number of decimal places.
+                uint numberOfDecimalPlaces = GetNumberOfDecimalPlaces();
+
+                // Calculate and show PI.
+                Console.WriteLine(HighPrecision.GetPi((int)numberOfDecimalPlaces));
+
+                // Find out if calculate another PI or exit.
+                if (!UserWantsMore())
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Method forces user to enter a valid input (number of decimal places) and returns it.
+        /// </summary>
+        /// <returns></returns>
+        private static uint GetNumberOfDecimalPlaces()
+        {
+            uint? numberOfDecimalPlaces = null;
+            do
+            {
+                Console.Write("Enter a number of decimal places: ");
+                numberOfDecimalPlaces = ValidateAndCastToNumber(Console.ReadLine());
+
+            } while (!numberOfDecimalPlaces.HasValue);
+
+            return numberOfDecimalPlaces.Value;
+        }
+
+        /// <summary>
+        /// Method forces user to enter a valid input (Y/y - for continue, N/n - for exit) and return it as a boolean.
+        /// </summary>
+        /// <returns></returns>
+        private static bool UserWantsMore()
+        {
+            bool? wantMore = null;
+            do
+            {
+                Console.Write("Want more? (y/n) ");
+                string answer = Console.ReadLine();
+                wantMore = CastAnswerToBool(answer != string.Empty ? answer[0] : '\0');
+            } while (!wantMore.HasValue);
+
+            return wantMore.Value;
+        }
+
+        /// <summary>
+        /// Method tries to parse and validate user's input and returns it if is valid.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static uint? ValidateAndCastToNumber(string value)
+        {
+            uint number;
+            if (uint.TryParse(value, out number))
+            {
+                if (number >= numberOfDecimalPlacesToWarnUser)
+                {
+                    bool? calculate = null;
+                    do
+                    {
+                        Console.WriteLine("Calculating PI with so many decimal places can take a long time depending on your hardware. Do you really want to calulcate it? (y/n)");
+                        string answer = Console.ReadLine();
+                        calculate = CastAnswerToBool(answer != string.Empty ? answer[0] : '\0');
+                    } while (!calculate.HasValue);
+
+                    if (!calculate.Value)
+                        return default(uint?);
+                }
+                return number;
+            }
+
+            return default(uint?);
+        }
+
+        /// <summary>
+        /// Method casts user's answer to nullable boolean (Y/y - true, N/n - false, otherwise - null)
+        /// </summary>
+        /// <param name="answer"></param>
+        /// <returns></returns>
+        private static bool? CastAnswerToBool(char answer)
+        {
+            if (acceptedAnswers.Contains(answer))
+            {
+                return answer.Equals('y') || answer.Equals('Y');
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
